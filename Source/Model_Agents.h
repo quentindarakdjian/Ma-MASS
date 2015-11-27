5fca8bb8022458664c84a1ee5aae85ae45ade287
Quentin Darakdjian <qdarakdjian@avenir-investir.fr>
2015-11-27 16:11:26 +0100

    Create Model_Agents.h

---
+++ Source/Model_Agents.h
@@ -0,0 +1,41 @@
+/*
+ * File:   Comp_Agent.h
+ * Author: jake
+ *
+ * Created on September 15, 2013, 4:21 PM
+ */
+
+#ifndef COMP_AGENT_H
+#define	COMP_AGENT_H
+#include <vector>
+#include <memory>
+#include "Agent.h"
+#include "StateMachine.h"
+#include "Zone.h"
+
+class Model_Agents{
+public:
+    Model_Agents();
+
+    void setup();
+    void step();
+    void setAgentGainsForZone(Zone *zone);
+    void setAgentWindowDecisionForZone(Zone *zone);
+    void setAgentLightDecisionForZone(Zone *zone);
+    void setAgentHeatingSetpointDecisionForZone(Zone *zone);
+    void setAgentBlindDecisionForZone(Zone *zone);
+    void setAgentCountForZone(Zone *zone);
+    void initialiseStates();
+    void setZones(std::vector<Zone> zones);
+    void postprocess();
+
+private:
+    void matchStateToZone(State &s);
+    std::vector<float> presenceProfile;
+    std::vector<Agent> population;
+    int stepCount;
+    StateMachine stateMachine; //StateMachine is included in Model_Agent
+    std::vector<Zone> zones;
+};
+
+#endif	/* COMP_AGENT_H */
