fd997fd408c13e73350eb77e464930207f7ab9d2
Quentin Darakdjian <qdarakdjian@avenir-investir.fr>
2015-11-27 16:26:50 +0100

    Create State_Out.h

---
+++ Source/State_Out.h
@@ -0,0 +1,15 @@
+#ifndef STATE_OUT_H
+#define STATE_OUT_H
+
+#include "./State.h"
+
+class State_Out: public State
+{
+    public:
+        State_Out();
+        bool hasZone() const;
+    protected:
+    private:
+};
+
+#endif // STATE_OUT_H
