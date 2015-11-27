26644f003bde4c81844efe6a238d02f84aa3fcf0
Quentin Darakdjian <qdarakdjian@avenir-investir.fr>
2015-11-27 16:24:16 +0100

    Create State_Cleaning.h

---
+++ Source/State_Cleaning.h
@@ -0,0 +1,15 @@
+#ifndef STATE_CLEANING_H
+#define STATE_CLEANING_H
+
+
+#include "./State.h"
+
+class State_Cleaning:public State
+{
+    public:
+        State_Cleaning();
+    protected:
+    private:
+};
+
+#endif // STATE_CLEANING_H
