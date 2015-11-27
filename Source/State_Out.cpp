b542385ff30b2e444324b483210a4a1184fee47f
Quentin Darakdjian <qdarakdjian@avenir-investir.fr>
2015-11-27 16:26:36 +0100

    Create State_Out.cpp

---
+++ Source/State_Out.cpp
@@ -0,0 +1,16 @@
+#include "State_Out.h"
+
+State_Out::State_Out()
+{
+    //ctor
+            id = 9; // out
+            metabolicRate = 0;
+            clo = 1;
+            activity = "Out";
+            location = "Out";
+
+}
+
+bool State_Out::hasZone() const{
+    return false;
+}
