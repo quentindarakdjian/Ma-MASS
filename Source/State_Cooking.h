758f350a03a6c3edb0c18031851cf49f60d1300b
Quentin Darakdjian <qdarakdjian@avenir-investir.fr>
2015-11-27 16:24:56 +0100

    Create State_Cooking.h

---
+++ Source/State_Cooking.h
@@ -0,0 +1,15 @@
+#ifndef STATE_COOKING_H
+#define STATE_COOKING_H
+#include "State.h"
+
+
+class State_Cooking : public State
+{
+    public:
+        State_Cooking();
+        
+    protected:
+    private:
+};
+
+#endif // STATE_COOKING_H
