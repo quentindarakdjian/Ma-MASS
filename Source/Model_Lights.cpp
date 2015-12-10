261c73270b4217eeec1897ada33d2c4d049253c1
Quentin Darakdjian <qdarakdjian@avenir-investir.fr>
2015-12-10 15:54:46 +0100

    Update Model_Lights.cpp

--- Source/Model_Lights.cpp
+++ Source/Model_Lights.cpp
@@ -37,12 +37,13 @@ bool Model_Lights::arrival(bool state, double Lumint){
         if(Lumint > 0 ){
             log10lumint = std::log10(Lumint);
         }
-
         if (log10lumint <= 0.843f) {
             probonarr = 1.f;
-        } else if (log10lumint >= 2.818f) {
+        }
+        else if (log10lumint >= 2.818f) {
             probonarr = 0.f;
-        } else {
+        }
+        else {
 
             float log10lumintm01arr = 0;
             if(Lumint - m01arr > 0 ){
@@ -53,7 +54,8 @@ bool Model_Lights::arrival(bool state, double Lumint){
         // usage of the probability to define the lights state
         if (Utility::randomDouble(0.0, 1.0) < probonarr) {
             currentLightState = 1.f;
-        } else {
+        }
+        else {
             currentLightState = 0.f;
         }
     }
@@ -73,7 +75,8 @@ bool Model_Lights::intermediate(bool state, double Lumint){
         float probonint = 0.f;
         if (Lumint == 0.f) {
             probonint = 1.f;
-        } else {
+        }
+        else {
             if(Lumint - d01int > 0 ){
                 probonint = a01int + b01int / (c01int * (log10(Lumint - d01int)));
             }
@@ -81,7 +84,8 @@ bool Model_Lights::intermediate(bool state, double Lumint){
         // usage of the probability to define the lights state
         if (Utility::randomDouble(0.0, 1.0) < probonint) {
             currentLightState = 1.f;
-        } else {
+        }
+        else {
             currentLightState = 0.f;
         }
     }
@@ -95,15 +99,20 @@ bool Model_Lights::departure(bool state, double Lumint, double futureDuration){
         float proboffdep;
         if (futureDuration < 30.f * 60.f) {
             proboffdep = 0.086f;
-        } else if (futureDuration < 60.f * 60.f) {
+        }
+        else if (futureDuration < 60.f * 60.f) {
             proboffdep = 0.314f;
-        } else if (futureDuration < 2.f * 60.f * 60.f) {
+        }
+        else if (futureDuration < 2.f * 60.f * 60.f) {
             proboffdep = 0.380f;
-        } else if (futureDuration < 4.f * 60.f * 60.f) {
+        }
+        else if (futureDuration < 4.f * 60.f * 60.f) { //4 heures
             proboffdep = 0.600f;
-        } else if (futureDuration < 12.f * 60.f * 60.f) {
+        }
+        else if (futureDuration < 12.f * 60.f * 60.f) { //12 heures
             proboffdep = 0.960f;
-        } else {
+        }
+        else {
             proboffdep = 1.f;
         }
         // NOTE: These probabilities are based on Pigg, Eilers, Reed, Behavioral Aspects of Lighting
@@ -111,7 +120,8 @@ bool Model_Lights::departure(bool state, double Lumint, double futureDuration){
         // usage of the probability to define the lights state
         if (Utility::randomDouble(0.0, 1.0) < proboffdep) {
             currentLightState = 0.f;
-        } else {
+        }
+        else {
             currentLightState = 1.f;
         }
     }
